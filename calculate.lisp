f4467e904d1432304e718394814e43861cc6514d
Ruan <ruan@pysoft.co.za>
2026-03-18 12:53:01 +0200

    Get basic expression evaluation up and running

--- calculate.lisp
+++ calculate.lisp
@@ -1,14 +1,50 @@
 (include "utils.lisp")
 (include "state.lisp")
 
+(defn map (fn list)
+  (if list
+    (cons (fn (head list)) (map fn (tail list)))
+  )
+)
+
+(defn call.function (fn args)
+  (cond
+    ((= fn "+") (call + args))
+    ((= fn "-") (call - args))
+    ((= fn "*") (call * args))
+    ((= fn "juxtapose") (call * args))
+    ((= fn "/") (call / args))
+    ((= fn "**") (call ** args))
+    ((= fn "%") (call % args))
+  )
+)
+
+(defn fetch.variable (var)
+  (tmpfn (var vars)
+    (if vars
+      (if (= var (head (head vars)))
+        (scd (head vars))
+        (rec var (tail vars))
+      )
+      (do
+        (println (&$ "Undefined variable " var "!"))
+        (exit 1)
+      )
+    )
+    (var variables)
+  )
+)
+
 (defn calculate (expr)
   (cond
     ((= (type expr) ' number)
-      number
+      expr
     )
     ((= (type expr) ' list)
-      (println "calculating expression:" expr)
-      0
+      (call.function (head expr) (map calculate (tail expr)))
+    )
+    ((= (type expr) ' string)
+      (fetch.variable expr)
     )
     (T
       (println "something went wrong! D:")
